89292d4c6adcd5abaa329da6266dacf948708352
Jibux <jibux@jibux.info>
2014-05-25 21:08:20 +0200

    ben ca avance ;-)

--- getData.pl
+++ getData.pl
@@ -2,47 +2,78 @@
 
 use strict;
 
-
-# Supprimer <a > et récuppérer le contenu
-# Supprimer <abbr> et //
-# Supprimer <i>
-# Mettre à la ligne les .+<b>
+use Data::Dumper;
 
 
-my $file = $ARGV[0];
-
-open(FILE, "<$file") or die "Cannot open $file\n";
-
-while(<FILE>) {
-	my $line = $_;
-
-	chomp $line;
-
-	if($line =~ m/^<div class="BD(sous|)titre">([^<]+)</) {
-		my $categorie = $2;
-		print "$categorie\n";
-	}
-
-	if($line =~ m/^<b>([^<>]+)<\/b>([^<>]+)/) {
-		my $caracName = tidyData($1);
-		my $caracVal = tidyData($2);
-
-		print "Name:\t$caracName --- Val: $caracVal\n";
-	}
-
-	if($line =~ m/^[^<]+/) {
-		print "Line:\t$line\n";
-	}
+if($ARGV[0] ne '') {
+	getData($ARGV[0]);
+} else {
+	print "HELLO\n";
 }
 
-close(FILE);
+sub getData {
+	my $file = shift;
+	my $items = {};
+	
+	$items->{'text'} = '';
+	$items->{'sorts'} = '';
+	
+	open(FILE, "<$file") or die "Cannot open $file\n";
+	open(OUT, ">>monsters.csv");
+	
+	my $type = 'text';
+	my $title = '';
+	
+	while(<FILE>) {
+		my $line = $_;
+	
+		chomp $line;
+	
+		if($line =~ m/^<div class="BDtitre">([^<]+)</) {
+			$title = $1;
+			#print OUT "\n\"$title";
+			$items->{'title'} = $title;
+		} elsif($line =~ m/^<div class="(BDsoustitre|box)">([^<]+)</) {
+			my $category = $2;
+			#print "$category\n";
+		} elsif($line =~ m/^<div class="BDtexte">/) {
+			$type = 'text';
+		} elsif($line =~ m/^<div class="BDsort/) {
+			$type = 'sorts';
+		} elsif($line =~ m/^<div class="([^"])">/) {
+			print "$title: Where is $1\n";
+		} elsif($line =~ m/^<b>([^<]+)<\/b>(.+)/) {
+			my $caracName = tidyData($1);
+			my $caracVal = tidyData($2);
+	
+			$items->{"$caracName"} = $caracVal;
+	
+			#print "Name:\t$caracName --- Val: $caracVal\n";
+			#print OUT "\",\"$caracVal";
+		} elsif($line =~ m/^[^<]+/) {
+			#print "Line:\t$line\n";
+			#print OUT "\n$line";
+	
+			if($type eq 'text' or $type eq 'sorts') {
+				$items->{"$type"} .= "*$line";
+			} else {
+				print "$title: Where are we?\n";
+				# Do nothing
+			}
+		}
+	}
+	
+	close(OUT);
+	close(FILE);
 
+	#print Dumper $items;
+}
 
 sub tidyData {
 	my $data = shift;
 
 	$data =~ s/^\s+//g;
-	$data =~ s/\s*[,;.]\s*$//g;
+	$data =~ s/\s*[,;.]*\s*$//g;
 
 	return $data;
 }
